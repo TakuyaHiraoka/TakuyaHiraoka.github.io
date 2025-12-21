fa299db9f641a8847984b78d678685b9639eddc2
takuya-h <hiraoka.et.al@gmail.com>
2025-12-21 14:48:00 +0900

    page style optim./update + publication update

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@
 =================================================================== */
 $(function(){
    // #で始まるアンカーをクリックした場合に処理
-   $('a[href^=#]').click(function() {
+   $('a[href^="#"]').click(function() {
       // スクロールの速度
       var speed = 400;// ミリ秒
       // アンカーの値取得
@@ -15,7 +15,7 @@ $(function(){
       // 移動先を数値で取得
       var position = target.offset().top;
       // スムーススクロール
-      $($.browser.safari ? 'body' : 'html').animate({scrollTop:position}, speed, 'swing');
+      $('html, body').animate({scrollTop:position}, speed, 'swing');
       return false;
    });
 });
